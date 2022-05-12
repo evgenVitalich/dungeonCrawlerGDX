20c37dcdfb67cc40848605df77ba21612cf79dd0
evgenVitalich <kharchenko_e_v@mail.ru>
2022-05-12 17:18:48 +0300

    Add actor class(personaj) Add new texture Update Starter class

---
+++ core/src/com/dich/dc/Personaj.java
@@ -0,0 +1,36 @@
+package com.dich.dc;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Personaj {
+
+    private final Vector2 pos = new Vector2();
+
+    private final Texture texture;
+
+    private int HP;
+
+    private int MoveSpd;
+
+
+
+    public Personaj(float x, float y) {
+
+        this.texture = new Texture("unnamed.png");
+        pos.set(x, y);
+
+    }
+
+    public void render(Batch batch){
+        batch.draw(texture, pos.x, pos.y);
+    }
+    public void dispose(){
+
+        texture.dispose();
+
+    }
+
+
+}
--- core/src/com/dich/dc/Starter.java
+++ core/src/com/dich/dc/Starter.java
@@ -8,24 +8,28 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Starter extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Personaj me;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+
+		me = new Personaj(100, 100);
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		me.render(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		me.dispose();
 	}
 }
