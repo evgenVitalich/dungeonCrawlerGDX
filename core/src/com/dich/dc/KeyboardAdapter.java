f17f175406c2415dbc54afa08e1cf5546735a527
0KolL0 <frecrafter@gmail.com>
2022-05-14 19:28:36 +0300

    Добавлена функция управления вектором

--- core/src/com/dich/dc/KeyboardAdapter.java
+++ core/src/com/dich/dc/KeyboardAdapter.java
@@ -33,4 +33,46 @@ public class KeyboardAdapter extends InputAdapter {
 
         return false;
     }
+//Принимает на вход управляемый вектор и T/F по клавишам WASD
+    public Vector2 dir(Vector2 dirinp, boolean LP, boolean RP, boolean UP, boolean DP){
+        Vector2 diropt = new Vector2(0,0);
+        if (LP) {
+            diropt.add(-1,0);
+            dirinp.set(diropt);
+        }
+
+        if (RP){
+            diropt.add(1,0);
+            dirinp.set(diropt);
+        }
+        if (UP){
+            diropt.add(0,1);
+            dirinp.set(diropt);
+        }
+        if (DP){
+            diropt.add(0,-1);
+            dirinp.set(diropt);
+        }
+        if (LP&UP) {
+            diropt.add(-1,1);
+            diropt.nor();
+            dirinp.set(diropt);
+        }
+        if (RP&UP){
+            diropt.add(1,1);
+            diropt.nor();
+            dirinp.set(diropt);
+        }
+        if (RP&DP){
+            diropt.add(1,-1);
+            diropt.nor();
+            dirinp.set(diropt);
+        }
+        if (LP&DP){
+            diropt.add(-1,-1);
+            diropt.nor();
+            dirinp.set(diropt);
+        }
+        return dirinp;
+    }
 }
